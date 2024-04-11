6b5cb08e14ceeab0c0ec3e2b6c22d9dd0919ce3e
Usrbotbot07 <160359608+Usrbotbot07@users.noreply.github.com>
2024-04-11 17:14:16 +0530

    Update left.py

--- VIPMUSIC/plugins/tools/left.py
+++ VIPMUSIC/plugins/tools/left.py
@@ -31,11 +31,11 @@ import random
 import asyncio
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/bb6c83c226deb16f28a10.jpg",
+    "https://telegra.ph/file/7276fa2ab5ccb84c6f19d.jpg",
+    "https://telegra.ph/file/a5a966471677c05b40c88.jpg",
+    "https://telegra.ph/file/a02a1e3a40c9b60e3a65e.jpg",
+    "https://telegra.ph/file/cac53df7f8e48ab036ebf.jpg",
 ]
 
 bg_path = "VIPMUSIC/assets/userinfo.png"
